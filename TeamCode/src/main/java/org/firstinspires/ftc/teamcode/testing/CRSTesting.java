ab75d316a4b57aabf88c5c318c0ba069ba700498
DeltaRobotics-FTC <DeltaRoboticsFTC@gmail.com>
2024-01-05 15:08:16 -0600

    made testing opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/CRSTesting.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@TeleOp(name="CRSTesting")
+@Disabled
+
+public class CRSTesting extends LinearOpMode{
+
+    public CRServo finger = null;
+    public void runOpMode() throws InterruptedException {
+
+        finger = hardwareMap.crservo.get("finger");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+
+            if (gamepad1.right_bumper){
+                finger.setPower(1);
+            }
+            else if (gamepad1.left_bumper){
+                finger.setPower(0);
+            }
+
+        }
+    }
+}
